7e77d7bf9c998320c8a7fe151739154f55e4ef46
Anrey lemin <anlemin@mail.ru>
2021-03-22 10:48:02 +0300

    Add Generator class

---
+++ TextureStreamer/src/Generator.h
@@ -0,0 +1,16 @@
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+
+class Generator
+{
+    public:
+        Generator();
+        virtual ~Generator();
+
+    protected:
+
+    private:
+};
+
+#endif // GENERATOR_H
