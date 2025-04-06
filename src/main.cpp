86180175ad36514590cfd1bbf4f5897f00ef39fc
Andrey Lemin <anlemin@mail.ru>
2025-04-07 01:31:18 +0300

    Add darkening

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@
 #include "ChanceBasedDrawer.h"
 #include "IPointsTraverser.h"
 #include "ITopology.h"
+#include "Filters.h"
 #include "PointsTraverserType.h"
 #include "IContainer.h"
 #include "ContainerType.h"
@@ -218,6 +219,8 @@ int main(int argc, char* argv[])
 				<< " Y=" << mouseY
 				<< '\r\n';
 		}
+
+		Darken(backgroundField, 0.99f);
 
 		dotsToDraw += m_dotsPerStep;
 
